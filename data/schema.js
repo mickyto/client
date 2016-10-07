1fa6e11558eb85564624c68a851501fd95b2830a
Igor Egai <mickyto@Igors-MacBook-Pro.local>
2016-10-07 22:09:47 +0300

    Add category to schema

--- data/schema.js
+++ data/schema.js
@@ -22,7 +22,12 @@ import {
 
 import {
     vendorModel,
-    vendorSchema
+    vendorSchema,
+    categoryModel,
+    productModel,
+    unitModel,
+    propertyModel
+
 } from './mongooseModels';
 
 
@@ -50,7 +55,6 @@ const {nodeInterface, nodeField} = nodeDefinitions(
     }
 );
 
-
 const VendorType = new GraphQLObjectType({
     name: 'Vendor',
     fields: () => ({
@@ -71,7 +75,6 @@ const VendorType = new GraphQLObjectType({
     interfaces: [nodeInterface]
 });
 
-
 const getVendors = new GraphQLObjectType({
     name: 'Vendors',
     fields: () => ({
@@ -90,11 +93,200 @@ const getVendors = new GraphQLObjectType({
     })
 });
 
+const UnitType = new GraphQLObjectType({
+    name: 'UnitType',
+    fields: {
+        id: {
+            type: GraphQLString
+        },
+        abbreviation: {
+            type: GraphQLString
+        }
+    }
+});
+
+const DefaultValueType = new GraphQLObjectType({
+    name: 'DefaultValueType',
+    fields: {
+        id: {
+            type: GraphQLString
+        },
+        name: {
+            type: GraphQLString
+        }
+    }
+});
+
+const PropertyType = new GraphQLObjectType({
+    name: 'PropertyType',
+    fields: {
+        id: {
+            type: GraphQLString
+        },
+        type: {
+            type: GraphQLString
+        },
+        name: {
+            type: GraphQLString
+        },
+        default_values: {
+            type: new GraphQLList(DefaultValueType),
+            resolve({ default_values }) {
+                return default_values;
+            }
+        }
+    }
+});
+
+const SpecificationType = new GraphQLObjectType({
+    name: 'SpecificationType',
+    fields: {
+        id: {
+            type: GraphQLString
+        },
+        value: {
+            type: GraphQLString
+        },
+        unit: {
+            type: UnitType,
+            resolve({ unit_id }) {
+                return new Promise((resolve, reject) => {
+                    unitModel.findById(unit_id, (err, unit) => {
+                        if (err) reject(err);
+                        else resolve(unit);
+                    });
+                });
+            }
+        },
+        property: {
+            type: PropertyType,
+            resolve({ property_id }) {
+                return new Promise((resolve, reject) => {
+                    propertyModel.findById(property_id, (err, property) => {
+                        if (err) reject(err);
+                        else resolve(property);
+                    });
+                });
+            }
+        }
+    }
+});
+
+const FrontImageType = new GraphQLObjectType({
+    name: 'FrontImageType',
+    fields: {
+        src: {
+            type: GraphQLString
+        }
+    }
+});
+
+const ProductType = new GraphQLObjectType({
+    name: 'ProductType',
+    fields: {
+        id: {
+            type: GraphQLString
+        },
+        model: {
+            type: GraphQLString
+        },
+        description: {
+            type: GraphQLString,
+            resolve({ description }) {
+                return description
+            }
+        },
+        front_image: {
+            type: FrontImageType,
+            resolve({ images }) {
+                function findFrontImage(image) {
+                    return image.front_image === 'yes';
+                }
+                return images.find(findFrontImage);
+            }
+        },
+        images: {
+            type: new GraphQLList(FrontImageType),
+            resolve({ images }) {
+                return images;
+            }
+        },
+        vendor: {
+            type: VendorType,
+            resolve({ vendor_id }) {
+                return new Promise((resolve, reject) => {
+                    vendorModel.findById(vendor_id, (err, vendor) => {
+                        if (err) reject(err);
+                        else resolve(vendor);
+                    });
+                });
+            }
+        },
+        specifications: {
+            type: new GraphQLList(SpecificationType),
+            resolve({specifications}) {
+                return specifications;
+            }
+        }
+    }
+});
+
+const CategoryType = new GraphQLObjectType({
+    name: 'CategoryType',
+    fields: {
+        id: {
+            type: GraphQLID
+        },
+        name: {
+            type: GraphQLString
+        },
+        ico: {
+            type: GraphQLString
+        },
+        products: {
+            type: new GraphQLList(ProductType),
+            resolve(category) {
+                return new Promise((resolve, reject) => {
+                    productModel.find({ category_id: category._id }, (err, products) => {
+
+                        resolve(products);
+
+                    });
+                });
+            }
+        }
+    }
+});
+
+const getCategories = new GraphQLObjectType({
+    name: 'Categories',
+    fields: () => ({
+        categories: {
+            type: new GraphQLList(CategoryType),
+            resolve() {
+                return new Promise((resolve, reject) => {
+                    categoryModel.find((err, categories) => {
+                        if (err) reject(err);
+                        else resolve(categories);
+                    });
+                });
+            }
+        },
+        node: nodeField
+    })
+});
+
 const queryType = new GraphQLObjectType({
     name: 'Query',
     fields: () => ({
-        viewer: {
+        vendorViewer: {
             type: getVendors,
+            resolve: () => {
+                return ''
+            }
+        },
+        categoryViewer: {
+            type: getCategories,
             resolve: () => {
                 return ''
             }

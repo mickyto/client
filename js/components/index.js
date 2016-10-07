e8c03a57ad893d63752b1b823f198b30aae3bdc2
Igor Egai <mickyto@Igors-MacBook-Pro.local>
2016-10-07 12:34:44 +0300

    Add header

--- js/components/index.js
+++ js/components/index.js
@@ -2,19 +2,24 @@ import chalk from 'chalk';
 import React from 'react';
 import Relay from 'react-relay';
 
+import Header from './header';
+
 class StarWarsApp extends React.Component {
 
     render() {
         const { vendors } = this.props.viewer;
         return (
             <div>
-                {vendors.map(vendor => (
-                    <li key={vendor.__dataID__}>
-                        <h1>{vendor.vendorId}</h1>
-                        <h1>{vendor.name}</h1>
-                        <h1>{vendor.logotype}</h1>
-                    </li>
-                ))}
+                <Header title="Skukit Home"/>
+                <div>
+                    {vendors.map(vendor => (
+                        <li key={vendor.__dataID__}>
+                            <h1>{vendor.vendorId}</h1>
+                            <h1>{vendor.name}</h1>
+                            <h1>{vendor.logotype}</h1>
+                        </li>
+                    ))}
+                </div>
             </div>
         );
     }
